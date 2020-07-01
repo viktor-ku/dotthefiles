705dd66cbdc37ff8ff0219da51bd99ce355a151a
Viktor Kuroljov <viktor.konsta@gmail.com>
2020-07-01 16:32:38 +0300

    client_os: assume every linux distro is linux
    Of course we can split these into their own "target" in the future.

--- src/lib/client_os.rs
+++ src/lib/client_os.rs
@@ -12,7 +12,21 @@ pub enum Type {
 impl std::convert::From<&os_info::Type> for Type {
   fn from(os_info_type: &os_info::Type) -> Self {
     match os_info_type {
-      os_info::Type::Linux => Self::Linux,
+      os_info::Type::Linux
+      | os_info::Type::Redhat
+      | os_info::Type::RedHatEnterprise
+      | os_info::Type::Ubuntu
+      | os_info::Type::Pop
+      | os_info::Type::Debian
+      | os_info::Type::Arch
+      | os_info::Type::Centos
+      | os_info::Type::Fedora
+      | os_info::Type::SUSE
+      | os_info::Type::openSUSE
+      | os_info::Type::OracleLinux
+      | os_info::Type::Solus
+      | os_info::Type::Manjaro
+      | os_info::Type::Alpine => Self::Linux,
       os_info::Type::Macos => Self::Darwin,
       os_info::Type::Windows => Self::Win,
       _ => Self::Unknown,
