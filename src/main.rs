74973df10264541417408412207e3aea2636d3e9
Viktor Kuroljov <viktor.konsta@gmail.com>
2020-07-01 14:31:06 +0300

    refactor: drop `async_std` dependency

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use async_std::io;
+use std::io;
 use structopt::StructOpt;
 
 mod cli;
@@ -12,21 +12,21 @@ use lib::read_yaml;
 #[tokio::main]
 async fn main() -> io::Result<()> {
   let cli = Cli::from_args();
-  let config_path = cli.config.canonicalize().await?;
+  let config_path = cli.config.canonicalize()?;
 
-  if config_path.is_dir().await {
+  if config_path.is_dir() {
     panic!("config should point to the dotthefiles.json or similar mappings config");
   }
 
   let mut base_dir = config_path.clone();
   base_dir.set_file_name("");
 
-  let config: config::Config = read_yaml(&config_path).await?;
+  let config: config::Config = read_yaml(&config_path)?;
 
   let mapping = mapping::Mapping {
     base_dir: &base_dir,
     os_type: &os_info::get().os_type(),
-    home_dir: &dirs::home_dir().unwrap().into(),
+    home_dir: &dirs::home_dir().unwrap(),
   };
 
   let files = mapping.map(&config)?;
