c94a0e540ec517d6948527dcf6abf5e5d0cd37a0
Viktor Kuroljov <viktor.konsta@gmail.com>
2020-07-03 22:29:09 +0300

    feat(main): add --list option to print out dotfiles it would link (like a dry-run)
    Can be used as a standalone tool, in case user wants to link files using sometning else
    and just needs to know the map in the form of from-to.

--- src/cli.rs
+++ src/cli.rs
@@ -6,4 +6,11 @@ use structopt::StructOpt;
 pub struct Cli {
   #[structopt(name = "config", parse(from_os_str))]
   pub config: PathBuf,
+
+  #[structopt(
+    short = "l",
+    long = "--list",
+    about = "prints to stdout all files it would link if it was an interactive mode without actually linking anything"
+  )]
+  pub list: bool,
 }
