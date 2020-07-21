6eeaa3766f746101fdd63430d2c4093cafc30147
Viktor Kuroljov <viktor.konsta@gmail.com>
2020-07-22 01:47:11 +0300

    chore: fmt

--- src/lib/dotfile.rs
+++ src/lib/dotfile.rs
@@ -1,5 +1,5 @@
+use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
-use serde::{Serialize, Deserialize};
 
 #[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct DotFile<'a> {
