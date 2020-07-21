436f4abe0fc17547d9e292111e9c2f0af54575e4
Viktor Kuroljov <viktor.konsta@gmail.com>
2020-07-22 01:50:33 +0300

    refactor: rename `app::Link` API name to 'ln'

--- src/app.rs
+++ src/app.rs
@@ -4,6 +4,7 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 #[structopt(name = "dtf")]
 pub enum App {
+  #[structopt(name = "ln")]
   Link {
     #[structopt(name = "config-path", parse(from_os_str))]
     config: PathBuf,
